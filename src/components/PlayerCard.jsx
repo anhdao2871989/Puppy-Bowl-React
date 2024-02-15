0ac7e9095442b218d24d3e58f4c0a91d555c901f
anhdao2871989 <anhdao2871989@gmail.com>
2024-02-15 16:08:28 -0600

    second commit

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { deletePlayer } from "../api";
-import styles from "../css/PlayerCard.css";
+
 
 
 export default function PlayerCard({ player }) {
